258eb67376408d57b37068c296532bcc5b995696
Nishan Chakma <nishan.chakma.nish@gmail.com>
2024-08-30 10:38:19 +1000

    add router index.js and fixed directory

---
+++ src/router/index.js
@@ -0,0 +1,65 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+
+import HomeView from '../views/HomeView.vue'
+import LoginView from '@/views/LoginView.vue'
+import AccessDeniedView from '@/views/AccessDeniedView.vue'
+import CommunityView from '../views/CommunityView.vue'
+import FindHelpView from '../views/FindHelpView.vue'
+import ProfileView from '../views/ProfileView.vue'
+import ResourcesView from '../views/ResourcesView.vue'
+import SettingsView from '../views/SettingsView.vue'
+import SignUp from '../views/SignUp.vue'
+
+// import { AuthService } from '@/services/auth'
+
+const routes = [
+    {
+        path: '/profile',
+        name: 'Profile',
+        component: ProfileView,
+        meta: { requiresAuth: true }
+    },
+    {
+        path: '/settings',
+        name: 'Settings',
+        component: SettingsView,
+        meta: { requiresAuth: true }
+    },
+    { path: '/home', name: 'Home', component: HomeView },
+    { path: '/login', name: 'Login', component: LoginView },
+    { path: '/signup', name: 'SignUp', component: SignUp },
+    { path: '/access-denied', name: 'AccessDenied', component: AccessDeniedView },
+    { path: '/community', name: 'Community', component: CommunityView },
+    { path: '/find-help', name: 'FindHelp', component: FindHelpView },
+    { path: '/resources', name: 'Resources', component: ResourcesView },
+    { path: '/', redirect: '/home' }, // Redirect to login by default
+]
+
+
+const router = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes
+})
+
+router.beforeEach((to, from, next) => {
+    /**
+     * Perform authentication checks before each route change.
+     *
+     * If the route to which we are navigating requires authentication, check if the user is logged in.
+     * If the user is not logged in, navigate to the login page instead.
+     * If the user is logged in, continue the navigation.
+     */
+    // console.log(AuthService.isLoggedIn())
+    // if (to.matched.some(record => record.meta.requiresAuth)) {
+    //     if (!AuthService.isLoggedIn()) {
+    //         next('/access-denied');
+    //     } else {
+    //         next();
+    //     }
+    // } else {
+    //     next();
+    // }
+});
+
+export default router;
---
+++ src/views/AccessDeniedView.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="access-denied">
+        <h1>Access Denied</h1>
+        <p>You do not have permission to view this page.</p>
+        <router-link to="/login">Go to Login</router-link>
+    </div>
+</template>
